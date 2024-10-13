fb5bdba79846e05aefdb58f786d20c1332c14e8b
T-101 <T-101@users.noreply.github.com>
2024-10-13 23:24:10 +0300

    Update goaccess_weekly.py
    Specify log format

--- goaccess_weekly.py
+++ goaccess_weekly.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     for site in SITES:
         week_ago = now - datetime.timedelta(weeks=1)
         outfile_name = f"{OUT_DIR}{week_ago.strftime('%Y')}/{week_ago.strftime('%Y-%W')}-[{date_start}-{date_end}]-{site}.html"
-        cmd = f"zcat -f {LOG_DIR}{site}.access.log* | grep '{date_pattern(now)}' | goaccess - -a -o {outfile_name} --html-report-title={site}"
+        cmd = f"zcat -f {LOG_DIR}{site}.access.log* | grep '{date_pattern(now)}' | goaccess - -a -o {outfile_name} --html-report-title={site} --log-format COMBINED"
         subprocess.run(cmd, shell=True)
         cmd = f"gzip -c {outfile_name} > {outfile_name}.gz && > {outfile_name}"
         subprocess.run(cmd, shell=True)
